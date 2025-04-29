ac0a08b7bfe0e8857f59c4a8ece8c7423c28fad1
Morten Andreas Myrstad <mortenam@uia.no>
2025-04-29 19:24:16 +0200

    Toggle theme uses wallpaper setter

--- hyprland/.config/hypr/scripts/toggle-theme.sh
+++ hyprland/.config/hypr/scripts/toggle-theme.sh
@@ -2,6 +2,7 @@
 
 THEME_FILE="$HOME/.config/hypr/theme"
 WALLPAPER_PATH=$(hyprctl hyprpaper listloaded)
+WALLPAPER_SETTER_SCRIPT="$HOME/.config/hypr/scripts/wallpaper.sh"
 
 # If THEME_FILE exists (default theme is dark) change contents to light if dark and vice verca
 if [[ -f "$THEME_FILE" ]]; then
@@ -9,15 +10,17 @@ if [[ -f "$THEME_FILE" ]]; then
     if [[ "$CURRENT_MODE" == "dark" ]]; then
         echo "Switching to light mode"
         echo "light" > "$THEME_FILE"
-        gsettings set org.gnome.desktop.interface color-scheme "prefer-light"
-        matugen image "$WALLPAPER_PATH" -m light --show-colors
+        #gsettings set org.gnome.desktop.interface color-scheme "prefer-light"
+        #matugen image "$WALLPAPER_PATH" -m light --show-colors
     else
         echo "Switching to dark mode"
         echo "dark" > "$THEME_FILE"
-        gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
-        matugen image "$WALLPAPER_PATH" -m dark --show-colors
+        #gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+        #matugen image "$WALLPAPER_PATH" -m dark --show-colors
     fi
 else
     echo "Theme file not found. Creating default theme file with dark mode."
     echo "dark" > "$THEME_FILE"
 fi
+
+$WALLPAPER_SETTER_SCRIPT "$WALLPAPER_PATH"
